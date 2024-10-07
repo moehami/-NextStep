478aa6d70c8755d65802ffb4fc39cf09a49eb352
moehami <m.aljaaly@yahoo.co.uk>
2024-10-07 04:40:19 +0300

    Update coming-soon.tsx

--- pages/coming-soon.tsx
+++ pages/coming-soon.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-import styles from '/styles/coming-soon.module.scss';
+import styles from '/styles/coming-soon.module.css';
 
 const ComingSoonPage = () => {
   return (
